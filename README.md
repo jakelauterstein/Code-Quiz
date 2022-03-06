fde0a19f00280df808f56adfa38a6134a9b0497c
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-03-06 16:32:59 -0600

    added README and screenshots

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Code-Quiz
+
+A simple quiz to test basic understanding of coding.
+
+## Description
+
+Upon clicking the 'Start Quiz' button, the user will be given a timed quiz.
+Click radio buttons to make selections, click submit quiz and scroll down to view score. 
+If the user does not finish in the time allotted, the score will be calculated based on available input with unmarked
+questions being marked incorrect.
+
+
+### Dependencies
+
+* Basic internet browser functionality
+
+
+### Executing program
+
+Press Start Quiz, click buttons next to answers, keep track of the time, press submit quiz, scroll down to view score.
+
+## Authors
+
+Jake Lauterstein
+with assistance from a mentor: Christian Ellsworth
+
+## Link to deployed Application
+
+https://jakelauterstein.github.io/Code-Quiz/
+
+## Screenshot 
+
+![full-site-screenshot](assets/images/start-screen.png)
+![full-site-screenshot](assets/images/quiz-results.png)
+
+## Acknowledgments
+
+https://simplestepscode.com/javascript-quiz-tutorial/
--- assets/js/script.js
+++ assets/js/script.js
@@ -61,33 +61,33 @@ generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
 function showQuestions(questions, quizContainer){
-// store the output and the answer choices
-var output = [];
-var answers;
+    // store the output and the answer choices
+    var output = [];
+    var answers;
 
 // for each question...
-for(var i=0; i<questions.length; i++){
+    for(var i=0; i<questions.length; i++){
 
 // first reset the list of answers
-answers = [];
+    answers = [];
 
 // for each available answer...
-for(letter in questions[i].answers){
+    for(letter in questions[i].answers){
 
 // ...add an html radio button
 answers.push(
-'<label>'
-+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-+ letter + ': '
-+ questions[i].answers[letter]
-+ '</label>'
+    '<label>'
+    + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+    + letter + ': '
+    + questions[i].answers[letter]
+    + '</label>'
 );
 }
 
 // add this question and its answers to the output
 output.push(
-'<div class="question">' + questions[i].question + '</div>'
-+ '<div class="answers">' + answers.join('') + '</div>'
+    '<div class="question">' + questions[i].question + '</div>'
+    + '<div class="answers">' + answers.join('') + '</div>'
 );
 }
 
@@ -135,7 +135,7 @@ showQuestions(questions, quizContainer);
 
 // on submit, show results
 submitButton.onclick = function(){
-showResults(questions, quizContainer, resultsContainer);
+    showResults(questions, quizContainer, resultsContainer);
 }
 
 var timerDisplay = document.getElementById("secondsLeft");
@@ -163,164 +163,3 @@ function decrement() {
 }
 
 }
-
-
-/*
-
-function buildQuiz (){
-    // create variable to store HTML output
-    const output = [];
-
-// for each question:
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-
-    // variable to store the list of possible answers
-    const answers = [];
-
-// and for each available answer
-for(letter in currentQuestion.answers){
-
-// add an HTML radio button
-answers.push(
-    `<label>
-    <input type="radio" name="questions${questionNumber}" value="${letter}">
-    ${letter} :
-    ${currentQuestion.answers[letter]}
-    </label>`
-);
-}
-
-// add question/answers to output
-
-output.push(
-    `<div class="question"> ${currentQuestion.question} </div>
-    <div class="answers"> ${answers.join('')} </div>`
-);
-}
-);
-// combine output list into HTML and display on the page
-
-quizContainer.innerHTML = output.join('');
-}
-
-function showResults(){
-
-    // gather answers
-    var answerContainers = quizContainer.querySelectorAll('.answers');
-    
-    // track user answers
-    var numCorrect = 0;
-    
-    // for each question
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-    
-        // find user selected answer
-       const answerContainer = answerContainers[questionNumber];
-       const selector = `input[name=question${questionNumber}]:checked`;
-       const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-    
-    // for correct answers
-    if(userAnswer === currentQuestion.correctAnswer){
-        // add to score
-        numCorrect++;
-    
-        //display correct answers in green
-        answerContainers[questionNumber].style.color = "lightgreen";
-    
-    } else {
-        // display incorrect answers pink
-        answerContainers[questionNumber].style.color = "pink";
-    }
-    });
-    
-    //show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-
-const quizContainer = document.getElementById("quiz")
-const resultsContainer = document.getElementById("results");
-const submitButton = document.getElementById("submit")
-
-const myQuestions = [
-    {
-        question: "Commonly used data types do NOT include:",
-        answers: {
-            a: "strings", 
-            b: "booleans", 
-            c: "alerts", 
-            d: "numbers"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "The condition in an if/else statement is enclosed with _______.",
-        answers: {
-            a:  "quotes",
-            b:  "curly brackets", 
-            c:  "parentheses", 
-            d:  "square brackets"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Arrays in Javascript can be used to store _________.",
-        answers: {
-            a: "numbers and strings",
-            b: "other arrays", 
-            c: "booleans", 
-            d: "all of the above"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "String values must be enclosed within _________ when being assigned to variables.",
-        answers: {
-            a: "commas",
-            b: "curly brackets", 
-            c: "quotes", 
-            d: "parentheses"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: {
-            a: "JavaScript",
-            b:  "terminal/bash", 
-            c:  "for loops", 
-            d:  "console.log"
-        },
-        correctAnswer: "d"
-    }
-];
-
-// display quiz
-buildQuiz();
-
-// show results
-submitButton.addEventListener("click", showResults);
-
-
-
-
-
-
-
-
-/*
-
-// Updates count on page
-function setCounterText() {
-  countEl.textContent = count;
-}
-// Attach event listener to increment button element
-incrementEl.addEventListener("click", function() {
-  count++;
-  setCounterText();
-});
-
-var startBtnEl = document.querySelector("#start-btn");
-var question = document.querySelector("#count");
-
-*/
